6d0c92b86c0345a1616133b288cd64f7e047ece3
slim <slimudoh@yahoo.com>
2019-06-21 11:19:37 +0100

    Update constant.js

--- src/global/constant.js
+++ src/global/constant.js
@@ -1,12 +1,1 @@
-export const UPLOAD_API =
-  "https://tm30vascmsapi.herokuapp.com/api/v1/fileupload";
-export const CATEGORIES_API =
-  "https://vas-product.herokuapp.com/products/api/v1/category";
-export const PRODUCT_API =
-  "https://vas-product.herokuapp.com/products/api/v1/products";
-export const CHANNEL_API =
-  "https://vas-product.herokuapp.com/products/api/v1/configurations/channels";
-export const CONTENT_API =
-  "https://vas-product.herokuapp.com/products/api/v1/configurations/contents";
-export const PLANS_API =
-  "https://vas-product.herokuapp.com/products/api/v1/plans";
+
